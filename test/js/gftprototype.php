04bdf1a7bd376c785e9b9cc8cc6619c5db427c58
Stefan Oderbolz <oderbolz@gmail.com>
2012-03-05 17:53:37 +0100

    First draft of JUnit XML reporting

--- test/js/gftprototype.php
+++ test/js/gftprototype.php
@@ -1,35 +1,10 @@
 <?php
-$value = shell_exec('DISPLAY=:99 phantomjs run_qunit.js http://gft.rdmr.ch/test/js/index.html');
-
-/*
-Possible output: 
-'waitFor()' finished in 2021ms.
-Tests completed in 1926 milliseconds.
-22 tests of 22 passed, 0 failed.
- */
-
-$lines = explode ("\n", $value);
-$timestamp = date("c");
-
-if (count($lines) >= 3) {
-	preg_match("/Tests completed in (\d*) milliseconds./", $lines[1], $timeRe);
-	$time = $timeRe[1] / 1000;
-	
-	preg_match("/(\d*) tests of (\d*) passed, (\d*) failed./", $lines[2], $testRe);
-	$tests = $testRe[2];
-	$failures = $testRe[3];
-	
-	
-	echo "<?xml version=\"1.0\"?>\n";
-	
-	echo "<!--\n";
-	echo $value;
-	echo "-->\n\n"; 
-	
-	echo "<testsuite name=\"GFTPrototype - JavaScript Tests\" timestamp=\"".$timestamp."\" hostname=\"localhost\" tests=\"".$tests."\" failures=\"".$failures."\" errors=\"0\" time=\"".$time."\">\n";
-	echo "<testcase name=\"phantomjs.tests\" classname=\"GFTPrototype - JavaScript Tests\" time=\"".$time."\"/>\n";
-	echo "</testsuite>\n";
-} else {
-	echo $value;
+/* Display 99 is configured on the server using Xvfb */
+$type = "";
+if (isset($_GET['type'])) {
+	$type = $_GET['type'];
 }
+$value = shell_exec("DISPLAY=:99 phantomjs run_qunit.js http://gft.rdmr.ch/test/js/index.html ".$type);
+echo $value;
+
 ?>
