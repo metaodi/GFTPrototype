8d8cabbacfb938a23dc926d71eb4400409b0efa0
Stefan Oderbolz <oderbolz@gmail.com>
2012-03-14 01:02:19 +0100

    Test for basic map integration with GFT-Layer

--- test/js/TestFusionTablesLayer.js
+++ test/js/TestFusionTablesLayer.js
@@ -1,32 +1,25 @@
 module("FusionTablesLayer");
-test("PaintSwitzerland", function() {
+test("Construct", function() {
 	var switzerland = new google.maps.LatLng(46.85, 8.7);
-
+	var layer = new google.maps.FusionTablesLayer();
+	
+	ok(layer instanceof google.maps.FusionTablesLayer,'Object should be of google.maps.FusionTablesLayer');
+	ok(switzerland instanceof google.maps.LatLng,'Object should be ofgoogle.maps.LatLng');
+});
+test("PaintMap", function() {
+	var switzerland = new google.maps.LatLng(46.85, 8.7);
 	var layer = new google.maps.FusionTablesLayer({
 		query: {
 			select: 'Location',
 			from: '2741123'
 		}
 	});
-	
-	ok(layer instanceof google.maps.FusionTablesLayer,'Object should be of google.maps.FusionTablesLayer');
-});
-
-
-function initialize() {
-	var switzerland = new google.maps.LatLng(46.85, 8.7);
-
-	var map = new google.maps.Map(document.getElementById('map_canvas'), {
+	var canvas = document.createElement("div");
+	var map = new google.maps.Map(canvas, {
 		center: switzerland,
 		zoom: 9,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	});
-
-	var layer = new google.maps.FusionTablesLayer({
-		query: {
-			select: 'Location',
-			from: '2741123'
-		}
-	});
 	layer.setMap(map);
-}
+	equal(canvas.getElementsByTagName("div").length,  1, "There should be a map on the canvas.")
+});
