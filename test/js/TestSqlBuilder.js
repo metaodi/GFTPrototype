9c800dced8d9052a47d71d01f2c57ecb4c46ac6a
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-03-23 16:10:06 +0100

    implemented example with oauth

--- test/js/TestSqlBuilder.js
+++ test/js/TestSqlBuilder.js
@@ -5,39 +5,49 @@ module("SqlBuilder", {
 	teardown: function(){}
 });
 
-test("Select (fields)", function() {
+test("SELECT (fields)", function() {
 	var gft = new GftLib;
 	raises(function() {
 		gft.select({fields:"*"});
 	}, "must throw error to pass");
 });
 
-test("Select (fields, table)", function() {
+test("SELECT (fields, table)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({fields:"Text, Number, Location", table:this.testGftTableId}), 'SELECT Text, Number, Location FROM ' + this.testGftTableId + ';');
 });
 
-test("Select (fields, table, condition)", function() {
+test("SELECT (fields, table, condition)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({table:this.testGftTableId, condition:"Text = 'Some record'"}), "SELECT * FROM " + this.testGftTableId + " WHERE Text = 'Some record';");
 });
 
-test("Select (fields, table, condition with AND)", function() {
+test("SELECT (fields, table, condition with AND)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({table:this.testGftTableId, condition:" AND Text = 'Some record'"}), "SELECT * FROM " + this.testGftTableId + " WHERE Text = 'Some record';");
 });
 
-test("Select (fields, table, condition, orderby)", function() {
+test("SELECT (fields, table, condition, orderby)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({table:this.testGftTableId, condition:"Text = 'Some record'", orderby:'text DESC'}), "SELECT * FROM " + this.testGftTableId + " WHERE Text = 'Some record' ORDER BY text DESC;");
 });
 
-test("Select (fields, table, condition, orderby, groupby)", function() {
+test("SELECT (fields, table, condition, orderby, groupby)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({table:this.testGftTableId, condition:"Text = 'Some record'", orderby:'text DESC', groupby:'Number'}), "SELECT * FROM " + this.testGftTableId + " WHERE Text = 'Some record' GROUP BY Number ORDER BY text DESC;");
 });
 
-test("Select (fields, table, condition, orderby, groupby, limit)", function() {
+test("SELECT (fields, table, condition, orderby, groupby, limit)", function() {
 	var sql = new SqlBuilder;
 	equal(sql.select({table:this.testGftTableId, condition:"Text = 'Some record'", orderby:'text DESC', groupby:'Number', limit:4}), "SELECT * FROM " + this.testGftTableId + " WHERE Text = 'Some record' GROUP BY Number ORDER BY text DESC LIMIT 4;");
+});
+
+test("DESCRIBE table", function() {
+	var sql = new SqlBuilder;
+	equal(sql.describe(this.testGftTableId), "DESCRIBE " + this.testGftTableId + ";");
+});
+
+test("INSERT", function() {
+	var sql = new SqlBuilder;
+	equal(sql.insert({table:this.testGftTableId, fields:"Text, Number", values:"'Some record', 123"}), "INSERT INTO " + this.testGftTableId + " ( Text, Number ) VALUES ( 'Some record', 123 );");
 });
