59738f8a28102e4a21b81a2f6daba39d805ebcec
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-04-15 12:13:50 +0200

    added marker icons

--- usecases/fixmystreet/app/view/ReportContainer.js
+++ usecases/fixmystreet/app/view/ReportContainer.js
@@ -3,24 +3,38 @@ Ext.define('FixMyStreet.view.ReportContainer', {
 	alias: 'widget.reportcontainer',
 
 	config: {
+		title: 'Melden',
+		iconCls: 'locate',
 		layout: 'vbox',
 		items: [
 			{
 				xtype: 'selectfield',
+				name: 'type',
 				label: 'Defekt melden...',
 				options: [
-					{text: 'Strassenlampe',  value: 'first'},
-					{text: 'Schlagloch', value: 'second'},
-					{text: 'Littering',  value: 'third'}
+					{text: 'Strassenlampe',  value: 'light'},
+					{text: 'Schlagloch', value: 'bump'},
+					{text: 'Littering',  value: 'littering'},
+					{text: 'Eisglaette',  value: 'ice'}
 				]
 			},
 			{
 				xtype: 'map',
 				id: 'gftmap',
+				// fill the remaining space with the map
+				flex: 1,
 				mapOptions: {
-					zoom: 17
+					zoom: 17,
+					streetViewControl: false,
+					navigationControl: false
 				},
 				useCurrentLocation: true
+			},
+			{
+				xtype: 'textfield',
+				label: 'Adresse',
+				name: 'address',
+				disabled: true
 			},
 			{
 				xtype: 'button',
@@ -29,5 +43,4 @@ Ext.define('FixMyStreet.view.ReportContainer', {
 			}
 		]
 	}
-	
 });
