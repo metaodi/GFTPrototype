774f4d4db470e9f4b2db48668257056a2f2aa687
Stefan Oderbolz <oderbolz@gmail.com>
2012-04-29 01:25:14 +0200

    Separate file for JavaScript test helper methods

--- test/js/helper.js
+++ test/js/helper.js
@@ -1,3 +1,13 @@
+function getDateString() {
+	var d = new Date();
+	return ('0' + d.getDate()).slice(-2) + '-'
+			+ ('0' + (d.getMonth()+1)).slice(-2) + '-'
+			+ d.getFullYear() + ' '
+			+ ('0' + (d.getHours())).slice(-2) + ':'
+			+ ('0' + (d.getMinutes())).slice(-2) + ':'
+			+ ('0' + (d.getSeconds())).slice(-2);
+}
+
 function getUrlParams() {
 	var params = {};
 	var urlParts = location.href.match(/\?(.*)$/i);
--- test/js/lib/TestGftLib.js
+++ test/js/lib/TestGftLib.js
@@ -7,16 +7,6 @@ module("GftLib", {
 	teardown: function(){}
 });
 
-function getDateString() {
-	var d = new Date();
-	return ('0' + d.getDate()).slice(-2) + '-'
-			+ ('0' + (d.getMonth()+1)).slice(-2) + '-'
-			+ d.getFullYear() + ' '
-			+ ('0' + (d.getHours())).slice(-2) + ':'
-			+ ('0' + (d.getMinutes())).slice(-2) + ':'
-			+ ('0' + (d.getSeconds())).slice(-2);
-}
-
 test("Construtor", function() {
 	ok(this.gft instanceof GftLib, 'Object should be of GftLib or one of it\'s childs');
 });
