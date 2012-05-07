04c42aa81c8c2e2276675c78c0b74bf50558fc8f
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-05-07 11:17:15 +0200

    commented overriden sencha touch functions

--- usecases/fixmystreet/app/plugin/PullRefresh.js
+++ usecases/fixmystreet/app/plugin/PullRefresh.js
@@ -23,6 +23,10 @@ Ext.define('FixMyStreet.plugin.PullRefresh', {
         ].join('')
 	},
 	
+	/**
+	 * OVERRIDEN SENCHA TOUCH FUNCTION
+	 * CHANGE: German date format
+	 */ 
 	resetRefreshState: function() {
         var me = this;
 
@@ -30,6 +34,39 @@ Ext.define('FixMyStreet.plugin.PullRefresh', {
         me.lastUpdated = new Date();
 
         me.setViewState('pull');
+		// CHANGE: German date format
         me.updatedEl.setHtml(Ext.util.Format.date(me.lastUpdated, "d.m.Y H:i:s"));
+    },
+	
+	/**
+	 * OVERRIDEN SENCHA TOUCH FUNCTION
+	 * CHANGE: wait for a longer time to scroll to top
+	 */
+	loadStore: function() {
+        var me = this,
+            list = me.getList(),
+            scroller = list.getScrollable().getScroller();
+
+        me.setViewState('loading');
+        me.isReleased = false;
+
+        Ext.defer(function() {
+            scroller.on({
+                scrollend: function() {
+                    if (me.getRefreshFn()) {
+                        me.getRefreshFn().call(me, me);
+                    } else {
+                        me.fetchLatest();
+                    }
+                    me.resetRefreshState();
+                },
+                delay: 100,
+                single: true,
+                scope: me
+            });
+            scroller.minPosition.y = 0;
+            scroller.scrollTo(null, 0, true);
+		// CHANGE: wait for a longer time to scroll to top
+        }, 1000, me);
     }
 });
