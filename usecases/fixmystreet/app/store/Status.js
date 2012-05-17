8a7d97f4f1b4748ab05c7ab7150c466aa564b33f
Juerg Hunziker <juerg.hunziker@gmail.com>
2012-05-17 17:45:14 +0200

    removed unused code

--- usecases/fixmystreet/app/store/Status.js
+++ usecases/fixmystreet/app/store/Status.js
@@ -4,9 +4,9 @@ Ext.define('FixMyStreet.store.Status', {
 		model: 'FixMyStreet.model.Status',
 		
 		data: [
-			{ key: 'new', value: 'Neu', sorting: 1 },
-			{ key: 'inprogress', value: 'In Arbeit', sorting: 2 },
-			{ key: 'done', value: 'Erledigt', sorting: 3 }
+			{ key: 'new', value: 'Neu' },
+			{ key: 'inprogress', value: 'In Arbeit' },
+			{ key: 'done', value: 'Erledigt' }
 		]
 	}
 });
