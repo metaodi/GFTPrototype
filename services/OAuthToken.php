d734b4d4bc9dfc07c9558f3ccd7089fe91c426ca
Stefan Oderbolz <oderbolz@gmail.com>
2012-08-12 11:34:39 +0200

    Updated OAuthToken service to match changes in new google-api-php-client

--- services/OAuthToken.php
+++ services/OAuthToken.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../lib/google-api-php-client/src/apiClient.php';
+require_once '../lib/google-api-php-client/src/Google_Client.php';
 
 function generate_jsonp($jsonString,$functionName="callback") 
 {
@@ -12,14 +12,17 @@ const FT_SCOPE = 'https://www.googleapis.com/auth/fusiontables';
 const SERVICE_ACCOUNT_NAME = '63601791805-gd5vj9a4pu177krdm9fu5rfhulkcl2bl@developer.gserviceaccount.com';
 const KEY_FILE = '/var/www/2cb665b201d20418fd491a37292a7d3cd8b57450-privatekey.p12';
 
-$client = new apiClient();
+$client = new Google_Client();
 $client->setApplicationName("GFTPrototype");
 $client->setClientId(CLIENT_ID);
 
 //add key
 $key = file_get_contents(KEY_FILE);
-$authScope = 'https://accounts.google.com/o/oauth2/token';
-$client->setAssertionCredentials(new apiAssertionCredentials(SERVICE_ACCOUNT_NAME, array(FT_SCOPE), $key));
+$client->setAssertionCredentials(new Google_AssertionCredentials(
+    SERVICE_ACCOUNT_NAME,
+    array(FT_SCOPE),
+    $key)
+);
 
 //reuse key if it's saved in the session
 session_start();
