7a6f39d8e245021caeaaf1c8aaac65997d41fb3c
Stefan Oderbolz <oderbolz@gmail.com>
2012-08-13 17:00:28 +0200

    Improved calendar for full-day events

--- examples/php/GFTCalendar.php
+++ examples/php/GFTCalendar.php
@@ -1,33 +1,40 @@
 <?php
 require_once 'FTOAuthServiceAccount.class.php';
+
+// the iCal date format
+const DATE_ICAL = 'Ymd\THis\Z';
+
+//Get data from Google Fusion Tables
 $auth = new FTOAuthServiceAccount("GFTCalendar");
 $service = $auth->getFTService();
-
 $selectQuery = "select rowid, Text, Date, Location, Number from 1AwxQ46kfmPoYoq38e5CopJOWkCo_9GUU_ucD6zI";
 $result = $service->query->sql($selectQuery);
 
 $rows = $result['rows'];
 $cols = $result['columns'];
- 
-// the iCal date format
-const DATE_ICAL = 'Ymd\THis\Z';
- 
+
 $ical = "BEGIN:VCALENDAR
 METHOD:PUBLISH
 VERSION:2.0
 PRODID:-//Stefan Oderbolz//GFTCalendar//EN\n";
  
-// loop over events
 foreach ($rows as $row) 
 {
+	$rowid = $row[array_search('rowid', $cols)];
+	$text = $row[array_search('Text', $cols)];
+	$location = $row[array_search('Location', $cols)];
+	$number = $row[array_search('Number', $cols)];
+	$startDate = new DateTime($row[array_search('Date', $cols)]);
+	$endDate = $startDate->add(DateInterval::createFromDateString('1 day'));
+	
 	$ical .= "BEGIN:VEVENT\n";
-	$ical .= "SUMMARY:" . $row[array_search('Text', $cols)] . "\n";
-	$ical .= "UID:" . $row[array_search('rowid', $cols)] . "\n";
+	$ical .= "SUMMARY:" . $text . "\n";
+	$ical .= "UID:" . $rowid . "\n";
 	$ical .= "STATUS:CONFIRMED\n";
-	$ical .= "DTSTART:" . date(DATE_ICAL, strtotime($row[array_search('Date', $cols)])) . "\n";
-	$ical .= "DTEND:" . date(DATE_ICAL, strtotime($row[array_search('Date', $cols)])) . "\n";
-	$ical .= "LOCATION:" . $row[array_search('Location', $cols)] . "\n";
-	$ical .= "SUMMARY:" . $row[array_search('Text', $cols)] . " (" . $row[array_search('Number', $cols)] . ")\n";
+	$ical .= "DTSTART:" . $startDate->format(DATE_ICAL) . "\n";
+	$ical .= "DTEND:" . $endDate->format(DATE_ICAL) . "\n";
+	$ical .= "LOCATION:" . $location . "\n";
+	$ical .= "SUMMARY:" . $text . " (" . $number . ")\n";
 	$ical .= "END:VEVENT\n";
 }
  
@@ -35,8 +42,15 @@ foreach ($rows as $row)
 $ical .= "END:VCALENDAR";
 
 //set correct content-type-header
-header('Content-type: text/calendar; charset=utf-8');
-header('Content-Disposition: inline; filename=gft_calendar.ics');
-echo $ical;
-exit;
+if (isset($_GET['debug'])) 
+{
+	echo "<pre>" . $ical . "</pre>";
+} 
+else 
+{
+	header('Content-type: text/calendar; charset=utf-8');
+	header('Content-Disposition: inline; filename=gft_calendar.ics');
+	echo $ical;
+	exit;
+}
 ?>
